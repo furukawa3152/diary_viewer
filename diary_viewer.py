fca5dba656d41b7241b5f1e4b35ecfce98cb7ca7
itaruchan <furukawa3152@gmail.com>
2023-11-23 21:09:48 +0900

    改行テスト

--- diary_viewer.py
+++ diary_viewer.py
@@ -50,13 +50,13 @@ for j in df_list:
     ymd = new_df_list[num][0]
     if j[0] == ymd:
         if j[1] != "":
-            hoge1 += j[1] +" \n "
+            hoge1 += j[1] +"\  n"
         if j[2] != "":
-            hoge2 += j[2] +" \n "
+            hoge2 += j[2] +"\  n"
         if j[3] != "":
-            hoge3 += j[3] +" \n "
+            hoge3 += j[3] +"\  n"
         if j[4] != "":
-            hoge4 += j[4] +" \n "
+            hoge4 += j[4] +"\  n"
     else:
         view_df_list.append([ymd, hoge1, hoge2, hoge3, hoge4])
         hoge1, hoge2, hoge3, hoge4 = ("", "", "", "")
@@ -64,13 +64,13 @@ for j in df_list:
         ymd = new_df_list[num][0]
         if j[0] == ymd:
             if j[1] != "":
-                hoge1 += j[1] + " \n "
+                hoge1 += j[1] + "\  n"
             if j[2] != "":
-                hoge2 += j[2] + " \n "
+                hoge2 += j[2] + "\  n"
             if j[3] != "":
-                hoge3 += j[3] + " \n "
+                hoge3 += j[3] + "\  n"
             if j[4] != "":
-                hoge4 += j[4] + " \n "
+                hoge4 += j[4] + "\  n"
 
 # for view in view_df_list:
 #     print(view)
@@ -85,5 +85,6 @@ viewer_df = pd.DataFrame(view_df_list,columns=["date","a","b","c","d"])
 
 
 # # Streamlitで表示
-st.title('Spreadsheet Data')
-st.write(viewer_df)
+# st.title('Spreadsheet Data')
+# st.write(viewer_df)
+print(viewer_df)
