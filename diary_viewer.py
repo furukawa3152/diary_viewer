8047cdf3822a112ef8fb0a2233c854156a64412b
furukawa3152 <furukawa3152`gmail.com>
2023-11-23 14:07:31 +0900

    zmlファイル隠す

--- diary_viewer.py
+++ diary_viewer.py
@@ -7,14 +7,15 @@ from google.oauth2.service_account import Credentials
 scope = ['https://www.googleapis.com/auth/spreadsheets', 'https://www.googleapis.com/auth/drive']
 # ダウンロードしたjsonファイル名をクレデンシャル変数に設定。
 #ローカル
-# credentials = Credentials.from_service_account_file("my-project-20230927-1db60d7e8989.json", scopes=scope)
+credentials = Credentials.from_service_account_file("venv/my-project-20230927-1db60d7e8989.json", scopes=scope)
 #デプロイ用
-credentials = Credentials.from_service_account_info( st.secrets["gcp_service_account"], scopes=[ "https://www.googleapis.com/auth/spreadsheets", ],)
+# credentials = Credentials.from_service_account_info( st.secrets["gcp_service_account"], scopes=[ "https://www.googleapis.com/auth/spreadsheets", ],)
+
 gc = gspread.authorize(credentials)
 
 # スプレッドシートIDを変数に格納する。
-SPREADSHEET_KEY = st.secrets["SPREADSHEET_KEY"]
-# SPREADSHEET_KEY = "1L65r8Dx8GZpn2qMyCU3V7hg2rZqfFZGMWcOm3j6qJ-0"
+# SPREADSHEET_KEY = st.secrets["SPREADSHEET_KEY"]
+SPREADSHEET_KEY = "1L65r8Dx8GZpn2qMyCU3V7hg2rZqfFZGMWcOm3j6qJ-0"
 # スプレッドシート（ブック）を開く
 workbook = gc.open_by_key(SPREADSHEET_KEY)
 
@@ -25,7 +26,28 @@ sheet = workbook.worksheet('シート1')
 data = sheet.get_all_values()
 # Pandas DataFrameに変換
 df = pd.DataFrame(data)
-#
+# 一行目をカラム名にする
+df.columns = df.iloc[0]
+# 一行目を削除する
+df = df[1:]
+df_list = df.values.tolist()
+# Streamlitで表示
+# st.title('Spreadsheet Data')
+# st.write(df)
+# 一列目を取得
+column_one = df.iloc[:, 0]
+
+# 重複を除去して新しいデータフレームを作成
+new_df = pd.DataFrame({df.columns[0]: column_one.unique()})
+
+# DataFrameをリストに変換
+new_df_list = new_df.values.tolist()
+
+
+
+
+
+
 # Streamlitで表示
 st.title('Spreadsheet Data')
-st.write(df)
+st.write(df_list)
