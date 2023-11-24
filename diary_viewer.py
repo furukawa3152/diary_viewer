683c78e3c610fed3d9bd9bfd8e59dcb16bdcbce8
itaruchan <furukawa3152@gmail.com>
2023-11-24 23:06:41 +0900

    color設定

--- diary_viewer.py
+++ diary_viewer.py
@@ -31,6 +31,9 @@ df.columns = df.iloc[0]
 # 一行目を削除する
 df = df[1:]
 st.title('Daily Log')
+
+st.subheader("Let's self-reflect!")
+
 with st.form("my_form", clear_on_submit=False):
     line_id = st.text_input('line_idを入力して下さい。')
     submitted = st.form_submit_button("日記を出力")
